a08d597b56156698ed3ac21138ad604bdba06ab5
Harry <henry.iz@yandex.ru>
2024-04-17 16:34:15 +0300

    added mysum

---
+++ sum_sum_sum_sum_02.py
@@ -0,0 +1,15 @@
+# def mysum(numbers):
+#     result = 0
+
+#     for item in numbers:
+#         result += item
+
+#     return result
+
+def mysum(numbers, start):
+    result = start
+
+    for item in numbers:
+        result += item
+
+    return result
